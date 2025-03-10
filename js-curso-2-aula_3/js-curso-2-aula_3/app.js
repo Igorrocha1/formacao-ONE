1f4e852c452fbceb419210e0249ec25e3080a004
Igorrocha1 <rocha-igor@hotmail.com>
2025-03-10 18:24:55 -0300

    Aula5 'Speech'

--- js-curso-2-aula_3/js-curso-2-aula_3/app.js
+++ js-curso-2-aula_3/js-curso-2-aula_3/app.js
@@ -7,6 +7,7 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
 function exibirMensagemInicial() {
@@ -65,10 +66,3 @@ function reiniciarJogo() {
     exibirMensagemInicial();
     document.getElementById('reiniciar').setAttribute('disabled', true)
 }
-
-
-
-
-
-
-
